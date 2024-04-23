f90dc6d0424641391afa22fad09a0ecafa2ab3bc
l-schier <lasse.10@hotmail.dk>
2024-04-23 14:25:38 +0200

    adds take_photo.sh

--- project/take_photo.sh
+++ project/take_photo.sh
@@ -1,2 +1,10 @@
-#!/bin/sh
-name=$(date
+#!/bin/bash
+name=$(date +%H%M%S_%N)
+dir=$(date +%F)
+
+if [ ! -d "${dir}" ]; then
+  mkdir "${dir}"
+fi
+
+rpicam-still -t 0.01 -o "./${dir}/${name:0:10}.jpg"
+
